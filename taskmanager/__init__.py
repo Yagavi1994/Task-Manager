820500bba7254644499324f974fb6e3843e42a95
Yagavi Kannadhasan <yagavi1994@gmail.com>
2024-07-30 21:37:00 +0000

    Error corrections.

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -12,10 +12,10 @@ if os.environ.get("DEVELOPMENT") == "True":
      app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
      app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+     app.config["SQLALCHEMY_DATABASE_URI"] = uri
      if uri.startswith("postgres://"):
          uri = uri.replace("postgres://", "postgresql://", 1)
-     app.config["SQLALCHEMY_DATABASE_URI"] = uri
-
+     
 db = SQLAlchemy(app)
 
 from taskmanager import routes  # noqa
