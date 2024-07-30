f9ef25f54fa69d57053feed5549e7fc904b1409b
Yagavi Kannadhasan <yagavi1994@gmail.com>
2024-07-30 21:20:56 +0000

    Error correction

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -11,7 +11,7 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 if os.environ.get("DEVELOPMENT") == "True":
      app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
-     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("postgres://udyjzjb2b96:YlgxPXBABK1R@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/coat_dusk_flick_239426")
+     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
      if uri.startswith("postgres://"):
          uri = uri.replace("postgres://", "postgresql://", 1)
      app.config["SQLALCHEMY_DATABASE_URI"] = uri
