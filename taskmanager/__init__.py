4e1612ff76288e07757a200f52e9896314fbf2d0
Yagavi Kannadhasan <yagavi1994@gmail.com>
2024-07-30 21:19:02 +0000

    Add files and codes for deployment,

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -7,7 +7,14 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+
+if os.environ.get("DEVELOPMENT") == "True":
+     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
+     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("postgres://udyjzjb2b96:YlgxPXBABK1R@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/coat_dusk_flick_239426")
+     if uri.startswith("postgres://"):
+         uri = uri.replace("postgres://", "postgresql://", 1)
+     app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
 db = SQLAlchemy(app)
 
